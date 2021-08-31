cb55fcdd040f4d65ad814ecf523c2c563d944137
danilbelyaev1 <danilbelyaev1@bk.ru>
2021-08-31 12:09:11 +0500

    add Timer

---
+++ src/zagotovki.js
@@ -0,0 +1,36 @@
+    // ТАЙМЕР
+    // let deadLine = '2021-08-1';
+    // function getTimeRemaning(endtime){
+    //     let t = Date.parse(endtime) - Date.parse(new Date()),
+    //         seconds = Math.floor((t/1000) % 60),
+    //         minutes = Math.floor((t/1000/60) % 60),
+    //         hours = Math.floor((t/(1000*60*60)));
+    //         return {
+    //             'total': t,
+    //             'hours': hours,
+    //             'minutes': minutes,
+    //             'seconds': seconds
+    //         };
+    // }
+
+    // function setClock(id, endtime) {
+    //     let timer = document.getElementById(id),
+    //         hours = timer.querySelector('.hours'),
+    //         minutes=timer.querySelector('.minutes'),
+    //         seconds=timer.querySelector('.seconds'),
+    //         timeInterval = setInterval(updateClock, 1000);
+    //     function updateClock(){
+    //         let t = getTimeRemaning(endtime);
+    //         hours.textContent = t.hours;
+    //         minutes.textContent = t.minutes;
+    //         seconds.textContent = t.seconds;
+
+    //         if (t.total <=0){
+    //             clearInterval(timeInterval);
+    //             hours.textContent = '00';
+    //             minutes.textContent = '00';
+    //             seconds.textContent = '00';
+    //         }
+    //     }
+    // }
+    // setClock('timer', deadLine);
