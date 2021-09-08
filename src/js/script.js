6cc8cd5c04987cfb7b286d434ac2735e78046cf1
danilbelyaev1 <danilbelyaev1@bk.ru>
2021-09-08 19:54:40 +0500

    sdfg

--- src/js/script.js
+++ src/js/script.js
@@ -156,9 +156,9 @@ window.addEventListener('DOMContentLoaded', () => {
         plusSlides(1);
     });
 
-    dotsWrap.addEventListener('click', function(event){
-        for (let i=0; i< dots.length + 1; i++){
-            if (event.target.classList.contains('dot') && event.target == dots[i-1]){
+    dotsWrap.addEventListener('click', function (event) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
                 currentSlide(i);
             }
         }
